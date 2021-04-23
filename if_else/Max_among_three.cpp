c0294bb7d8b0d993db666e2e5b04d497dc5bff57
shubhangi rai <66017315+Shubhangi-rai@users.noreply.github.com>
2021-04-23 22:56:56 +0530

    Add files via upload

---
+++ if_else/Max_among_three.cpp
@@ -0,0 +1,26 @@
+// Program to find the maximum among three numbers.
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+    if(n1>n2){
+       if(n1>n3){
+           cout<<n1<<endl;
+       }
+       else{
+           cout<<n3<<endl;
+       }
+    }
+    else{
+        if(n2>n3){
+           cout<<n2<<endl;
+        }
+        else{
+            cout<<n3<<endl;
+        }
+        
+    }
+   
+    return 0;
+}
