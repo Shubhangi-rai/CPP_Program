4a40c4c0b68c8a5de04b16dd832adb4cbc342965
shubhangi rai <66017315+Shubhangi-rai@users.noreply.github.com>
2021-04-23 22:30:36 +0530

    Add files via upload

---
+++ if_else/Even_or_Odd.cpp
@@ -0,0 +1,14 @@
+// Program to check if a given number is even or odd.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Number is even"<<endl;
+    }
+    else{
+        cout<<"Number is Odd"<<endl;
+    }
+    return 0;
+}
